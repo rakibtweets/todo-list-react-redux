abe0a0ef6a41cd6dbe07c43ca709c5bde33aabc3
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-01-01 22:51:39 +0600

    defining actions

---
+++ src/redux/todos/actions.js
@@ -0,0 +1,41 @@
+import {
+  ADDED, ALL_COMPLETED, COLORSELECTED, DELETED, TOGGLED
+} from './actionTypes';
+
+export const added = (todoText) => {
+  return {
+    type: ADDED,
+    payload: todoText,
+  };
+};
+
+export const toggled = (todoId) => {
+  return {
+    type: TOGGLED,
+    payload: todoId,
+  };
+};
+
+export const colorSelected = (todoId, color) => {
+  return {
+    type: COLORSELECTED,
+    payload: {
+      todoId,
+      color,
+    },
+  };
+};
+
+export const deleted = (todoId) => {
+  return {
+    type: DELETED,
+    payload: todoId,
+
+  };
+};
+
+export const allCompleted = () => {
+  return {
+    type: ALL_COMPLETED,
+  };
+};
