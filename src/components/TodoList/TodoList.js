cda38fd2ed19d1155bbe46d02b3d004b3dd93a5d
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-01-03 22:19:58 +0600

    todolist and todo ui done

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Todo from '../Todo/Todo';
 
 function TodoList() {
+  const todos = useSelector(((state) => state.todos));
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
       {/* <!-- todo --> */}
-      <Todo />
+      {
+        todos.map((todo) => <Todo key={todo.id} todo={todo} />)
+      }
 
     </div>
   );
--- src/redux/todos/initialState.js
+++ src/redux/todos/initialState.js
@@ -2,13 +2,20 @@ const initialState = [
   {
     id: 1,
     text: 'Learn React Js',
-    completed: true
+    completed: true,
+    color: 'yellow',
   },
   {
     id: 2,
     text: 'Learn Redux',
     completed: false,
     color: 'red'
+  },
+  {
+    id: 3,
+    text: 'Learn Mongoose',
+    completed: true,
+    color: 'green'
   },
 ];
 
