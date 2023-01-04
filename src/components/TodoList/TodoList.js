420d0d785203104e7348ed48a39ed42865b0bfac
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-01-04 18:54:43 +0600

    filter todo list

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,13 +2,35 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Todo from '../Todo/Todo';
 
+const filterStatus = (todo, status) => {
+  switch (status) {
+  case 'Complete':
+    return todo.completed;
+  case 'Incomplete':
+    return !todo.completed;
+  default:
+    return true;
+  }
+};
+
+const filterColor = (todo, colors) => {
+  if (colors.length > 0) {
+    return colors.includes(todo?.color);
+  }
+  return true;
+};
+
 function TodoList() {
   const todos = useSelector(((state) => state.todos));
+  const { status, colors } = useSelector(((state) => state.filters));
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
       {/* <!-- todo --> */}
       {
-        todos.map((todo) => <Todo key={todo.id} todo={todo} />)
+        todos
+          .filter((todo) => filterStatus(todo, status))
+          .filter((todo) => filterColor(todo, colors))
+          .map((todo) => <Todo key={todo.id} todo={todo} />)
       }
 
     </div>
