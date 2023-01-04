2dcbcbe46d2ded6961a124214e180190586c0c38
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-01-04 18:59:24 +0600

    completed task UI

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -44,7 +44,7 @@ function Todo({ todo }) {
         )}
       </div>
 
-      <div className="select-none flex-1 line-through">
+      <div className={`select-none flex-1 ${completed && 'line-through'}`}>
         {text}
       </div>
 
