d50172ac56b1797d482dbc5a693d997136307503
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-01-06 22:22:23 +0600

    todo title

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar() {
   return (
     <div
-      className="fixed top-0 left-0 text-center w-full header bg-green-600 py-6 text-white font-bold text-2xl shadow-lg"
+      className="fixed top-0 left-0 text-center w-full header bg-green-600 py-6 text-white font-bold text-3xl shadow-lg"
     >
       Simple Todo Application with Redux
     </div>
