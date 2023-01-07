45615fc96289c11d5d9325bb8e81adacc7014c81
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-01-07 23:04:39 +0600

    json server created

--- src/redux/todos/initialState.js
+++ src/redux/todos/initialState.js
@@ -1,22 +1,3 @@
-const initialState = [
-  {
-    id: 1,
-    text: 'Learn React Js',
-    completed: true,
-    color: 'yellow',
-  },
-  {
-    id: 2,
-    text: 'Learn Redux',
-    completed: false,
-    color: 'red'
-  },
-  {
-    id: 3,
-    text: 'Learn Mongoose',
-    completed: true,
-    color: 'green'
-  },
-];
+const initialState = [];
 
 export default initialState;
