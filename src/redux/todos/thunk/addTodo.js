a6b374d0e911e8034c33a89ef206108aa08df901
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-01-08 07:17:42 +0600

    thunk api call

---
+++ src/redux/todos/thunk/addTodo.js
@@ -0,0 +1,20 @@
+import { addedTodo } from '../actions';
+
+const addTodo = (todoText) => {
+  return async (disptach) => {
+    const response = await fetch('http://localhost:9000/todos', {
+      method: 'POST',
+      body: JSON.stringify({
+        text: todoText,
+        completed: false
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=utf-8'
+      }
+    });
+    const todo = await response.json();
+    disptach(addedTodo(todo.text));
+  };
+};
+
+export default addTodo;
